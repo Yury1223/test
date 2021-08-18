072d6f51102b4f42ea2e78268a446bb5745820da
Nemuru <noop>
2021-08-19 08:42:15 +0900

    todo

---
+++ components/TodoList.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <v-card
+      class="mx-auto"
+      max-width="344"
+      outlined
+      draggable=".todo"
+    >
+      <v-list-item two-line>
+      <v-list-item-content ng-if="todo">
+        <v-list-item-title class="text-h5 mb-1">
+        {{ todo.name }}
+        </v-list-item-title>
+        <v-list-item-subtitle>
+        {{ todo.description }}
+        </v-list-item-subtitle>
+      </v-list-item-content>
+      </v-list-item>
+
+      <v-card-actions>
+      <v-btn
+        outlined
+        rounded
+        text
+        v-if="type !== 'done'"
+        @click="onDone(type, index)"
+      >
+        Done
+      </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  import draggable from 'vuedraggable'
+
+  export default Vue.extend({
+    components: {
+      draggable
+    },
+    props: {
+      todo: {
+        type: Object,
+        required: true,
+        default: {
+          name: '',
+          description: ''
+        }
+      },
+      index: {
+        type: Number,
+        required: true,
+        default: 0
+      },
+      type: {
+        type: String,
+        required: true
+      },
+      onDone: {
+        type: Function,
+        required: true
+      } 
+    }
+  })
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -9,8 +9,10 @@
       </v-row>
       <v-row>
         <v-col cols="12" sm="6">
-          <v-text-field v-model="todo" label="Solo"></v-text-field>
-          <p>{{todo}}</p>
+          <v-text-field v-model="name" label="タスク名を入力してください" required></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="6">
+          <v-text-field v-model="description" label="内容を入力してください" required></v-text-field>
         </v-col>
       </v-row>
       <v-row>
@@ -22,32 +24,77 @@
 
     <v-container>  
       <v-row>
-        <v-col cols="12" sm="6">
-          <v-card
-            v-for="(todo, index) in todos" :key="todo"
-            class="mx-auto"
-            max-width="344"
-            outlined
-          >
-            <v-list-item one-line>
-              <v-list-item-content>
-                <div class="text-overline mb-4">
-                  {{ todo }}
-                </div>
-              </v-list-item-content>
-            </v-list-item>
+        <v-col cols="12" sm="4">
+          <h1>NotStarted</h1>
+          <draggable v-model="notStarted" draggable=".todo" group="todos">
+            <div class="todo" v-for="(todo, index) in notStarted" :key="index">
+              <TodoList
+                :todo="todo"
+                type="notStarted"
+                :onDone="onDone"
+                :index="index"
+              />
+            </div>
+          </draggable>
+        </v-col>
 
-            <v-card-actions>
-              <v-btn
-                outlined
-                rounded
-                text
-                @click="done(index)"
-              >
-                Done
-              </v-btn>
-            </v-card-actions>
-          </v-card>
+        <v-col cols="12" sm="4">
+          <h1>Doing</h1>
+          <draggable v-model="doing" draggable=".todo" group="todos">
+            <div class="todo" v-for="(todo, index) in doing" :key="index">
+              <TodoList
+                :todo="todo"
+                type="doing"
+                :onDone="onDone"
+                :index="index"
+              />
+            </div>
+          </draggable>
+          <!-- <draggable v-model="doing" draggable=".todo" group="todos">
+            <v-card
+              v-for="(todo, index) in doing" :key="index"
+              class="mx-auto todo"
+              max-width="344"
+              outlined
+              draggable=".todo"
+            >
+              <v-list-item two-line>
+                <v-list-item-content>
+                  <v-list-item-title class="text-h5 mb-1">
+                    {{ todo.name }}
+                  </v-list-item-title>
+                  <v-list-item-subtitle>
+                    {{ todo.description }}
+                  </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+
+              <v-card-actions>
+                <v-btn
+                  outlined
+                  rounded
+                  text
+                  @click="onDone('doing', index)"
+                >
+                  Done
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </draggable> -->
+        </v-col>
+
+        <v-col cols="12" sm="4">
+          <h1>Done</h1>
+          <draggable v-model="done" draggable=".todo" group="todos">
+            <div class="todo" v-for="(todo, index) in done" :key="index">
+              <TodoList
+                :todo="todo"
+                type="done"
+                :onDone="onDone"
+                :index="index"
+              />
+            </div>
+          </draggable>
         </v-col>
       </v-row> 
     </v-container>
@@ -57,19 +104,53 @@
 
 <script lang="ts">
   import Vue from 'vue'
+  import draggable from 'vuedraggable'
+  import TodoList from '../components/TodoList.vue'
+
+  export interface ToDo {
+    name: string
+    description: string
+  }
 
   export default Vue.extend({
     data: () => ({
-      todo: '',
-      todos: [] as string[]
+      name: '',
+      description: '',
+      notStarted: [] as ToDo[],
+      doing: [] as ToDo[],
+      done: [] as ToDo[]
     }),
+    components: {
+      draggable,
+      TodoList
+    },
     methods: {
       apply: function () {
-        this.todos.push(this.todo)
-        this.todo = ''
+        this.notStarted.push({
+          name: this.name,
+          description: this.description
+        })
+
+        this.name = '',
+        this.description = ''
       },
-      done: function (targetIndex: number = 0) {
-        this.todos = this.todos.filter((_, index) => {
+      onDone: function (type: 'notStarted' | 'doing' | 'done', targetIndex: number = 0) {
+        console.log(type, targetIndex)
+        switch (type) {
+          case 'notStarted':
+            this.done.push(this.notStarted[targetIndex])
+            this.notStarted = this._filter(this.notStarted, targetIndex)
+            break
+          case 'doing':
+            this.done.push(this.doing[targetIndex])
+            this.doing = this._filter(this.doing, targetIndex)
+            break
+        }        
+
+        this.$forceUpdate()
+      },
+      _filter: function (list: ToDo[], targetIndex: number) {
+        return list.filter((_, index) => {
           return index !== targetIndex
         })
       }
